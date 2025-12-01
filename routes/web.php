c5d1618a9db7992e9732368fbb08ab88567c4ca3
mintathr <keywly21@gmal.com>
2025-12-01 11:22:48 +0700

    20251201 under construction

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Facades\Route;
 /* Route::get('/', function () {
     return view('welcome');
 }); */
-/* Route::fallback(function () {
+Route::fallback(function () {
     return view('under-construction');
-}); */
+});
 
 
-Route::get('/', fn () => view('web-layout.pages.index'))->name('home');
+/* Route::get('/', fn () => view('web-layout.pages.index'))->name('home');
 Route::get('/cloudsuite-automotive', fn () => view('web-layout.pages.csa'))->name('page.csa');
 Route::get('/cloudsuite-industrial-enterprise', fn () => view('web-layout.pages.csie'))->name('page.csie');
 Route::get('/enterprise-asset-management', fn () => view('web-layout.pages.eam'))->name('page.eam');
@@ -24,4 +24,4 @@ Route::get('/about', fn () => view('web-layout.pages.about'))->name('about');
 Route::get('contact-us', fn () => view('web-layout.pages.contact'))->name('contact');
 
 Route::post('contact-us', [WebNimController::class, 'store'])->name('contact.store');
-
+ */
